09f27bb5d76067abd6f2a4d5fe43c0c6cee9a735
WUSO01 <Lovemyself55>
2017-05-03 10:11:40 +0800

    show

--- src/components/user.vue
+++ src/components/user.vue
@@ -1,11 +1,32 @@
 <template>
-  <div>test</div>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th v-for="th in thDatas">{{ th }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="td in tbody">
+          <td>{{ td }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 <script>
+import mockData from '../../test.json'
 export default {
   name: 'user',
   data () {
+    let datas = mockData
+    let thDatas = datas.splice(0, 1)
+    console.log(datas)
+    for (var i of thDatas) {
+    }
     return {
+      thDatas: i,
+      tbody: datas
     }
   }
 }
